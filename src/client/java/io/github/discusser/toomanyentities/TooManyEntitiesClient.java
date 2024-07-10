1ff7d4eb39fe6cfe084ff14bcd88fc937dbfdf4e
Discusser <47938380+Discusser@users.noreply.github.com>
2024-07-10 10:34:16 +0200

    Initial commit

---
+++ src/client/java/io/github/discusser/toomanyentities/TooManyEntitiesClient.java
@@ -0,0 +1,10 @@
+package io.github.discusser.toomanyentities;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TooManyEntitiesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
