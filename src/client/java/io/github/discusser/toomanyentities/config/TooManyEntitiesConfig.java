ff5404e16905c48741ddc660a74a80754a3c4140
Discusser <47938380+Discusser@users.noreply.github.com>
2024-07-10 14:33:15 +0200

    Get rid of previous config screen

--- src/client/java/io/github/discusser/toomanyentities/config/TooManyEntitiesConfig.java
+++ src/client/java/io/github/discusser/toomanyentities/config/TooManyEntitiesConfig.java
@@ -4,10 +4,6 @@ import io.github.discusser.toomanyentities.TooManyEntitiesClient;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import me.shedaniel.clothconfig2.impl.builders.BooleanToggleBuilder;
-import me.shedaniel.clothconfig2.impl.builders.IntFieldBuilder;
-import net.minecraft.text.Text;
 
 @Config(name = TooManyEntitiesClient.MODID)
 public class TooManyEntitiesConfig implements ConfigData {
@@ -16,18 +12,4 @@ public class TooManyEntitiesConfig implements ConfigData {
 
     @ConfigEntry.Gui.Excluded
     public static TooManyEntitiesConfig instance = null;
-
-    public static IntFieldBuilder createIntField(ConfigEntryBuilder entryBuilder, String fieldName, Integer field, int defaultValue) {
-        return entryBuilder
-                .startIntField(Text.translatable("option.too-many-entities." + fieldName), field)
-                .setTooltip(Text.translatable("option.too-many-entities." + fieldName + ".tooltip"))
-                .setDefaultValue(defaultValue);
-    }
-
-    public static BooleanToggleBuilder createBooleanToggle(ConfigEntryBuilder entryBuilder, String fieldName, Boolean field, boolean defaultValue) {
-        return entryBuilder
-                .startBooleanToggle(Text.translatable("option.too-many-entities." + fieldName), field)
-                .setTooltip(Text.translatable("option.too-many-entities." + fieldName + ".tooltip"))
-                .setDefaultValue(defaultValue);
-    }
 }
