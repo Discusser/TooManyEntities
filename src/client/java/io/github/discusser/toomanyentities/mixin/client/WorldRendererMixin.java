eb65a598454b4c9c7935033d5b1a550767fd3314
Discusser <47938380+Discusser@users.noreply.github.com>
2024-07-10 12:10:07 +0200

    Hide specific entity when there are too many

--- src/client/java/io/github/discusser/toomanyentities/mixin/client/WorldRendererMixin.java
+++ src/client/java/io/github/discusser/toomanyentities/mixin/client/WorldRendererMixin.java
@@ -1,11 +1,9 @@
 package io.github.discusser.toomanyentities.mixin.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import io.github.discusser.toomanyentities.TooManyEntities;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
