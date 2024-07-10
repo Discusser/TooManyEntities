89900d864b58e8febd88eb3559220629270d3cf0
Discusser <47938380+Discusser@users.noreply.github.com>
2024-07-10 12:08:35 +0200

    Hide specific entity when there are too many

---
+++ src/client/java/io/github/discusser/toomanyentities/mixin/client/WorldRendererMixin.java
@@ -0,0 +1,40 @@
+package io.github.discusser.toomanyentities.mixin.client;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import io.github.discusser.toomanyentities.TooManyEntities;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.HashMap;
+
+@Mixin(WorldRenderer.class)
+public class WorldRendererMixin {
+    @Unique
+    private final HashMap<Class<?>, Integer> entityCounts = new HashMap<>();
+
+    @Inject(method = "renderEntity", at = @At(value = "HEAD"), cancellable = true)
+    private void beforeEntityRender(CallbackInfo info, @Local(argsOnly = true) Entity entity) {
+        int MAX_ENTITY_COUNT = 128;
+        if (entityCounts.getOrDefault(entity.getClass(), 0) > MAX_ENTITY_COUNT) {
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/WorldRenderer;regularEntityCount:I", ordinal = 1, shift = At.Shift.AFTER))
+    private void afterEntityCountIncrement(CallbackInfo info, @Local Entity entity) {
+        Class<? extends Entity> klass = entity.getClass();
+        entityCounts.put(klass, entityCounts.getOrDefault(klass, 0) + 1);
+    }
+
+
+    @Inject(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/WorldRenderer;regularEntityCount:I", ordinal = 0))
+    private void afterEntityCountReset(CallbackInfo info) {
+        entityCounts.clear();
+    }
+}
