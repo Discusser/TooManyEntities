8839359cef8a9bf6590da582892e1b2b35927b3b
Discusser <47938380+Discusser@users.noreply.github.com>
2024-07-10 15:54:52 +0200

    Add custom AutoConfig GuiProvider for Map type

--- src/client/java/io/github/discusser/toomanyentities/TooManyEntitiesClient.java
+++ src/client/java/io/github/discusser/toomanyentities/TooManyEntitiesClient.java
@@ -1,11 +1,16 @@
 package io.github.discusser.toomanyentities;
 
+import io.github.discusser.toomanyentities.config.MapGuiProvider;
 import io.github.discusser.toomanyentities.config.TooManyEntitiesConfig;
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.gui.registry.GuiRegistry;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TooManyEntitiesClient implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("too-many-entities");
@@ -14,6 +19,8 @@ public class TooManyEntitiesClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		AutoConfig.register(TooManyEntitiesConfig.class, GsonConfigSerializer::new);
+		GuiRegistry registry = AutoConfig.getGuiRegistry(TooManyEntitiesConfig.class);
+		registry.registerTypeProvider(new MapGuiProvider(), Map.class);
 		TooManyEntitiesConfig.instance = AutoConfig.getConfigHolder(TooManyEntitiesConfig.class).getConfig();
 	}
 }
--- src/client/java/io/github/discusser/toomanyentities/config/TooManyEntitiesConfig.java
+++ src/client/java/io/github/discusser/toomanyentities/config/TooManyEntitiesConfig.java
@@ -4,12 +4,48 @@ import io.github.discusser.toomanyentities.TooManyEntitiesClient;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.entity.EntityType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Config(name = TooManyEntitiesClient.MODID)
 public class TooManyEntitiesConfig implements ConfigData {
-    public Integer maxEntityCount = 0;
+
+
+    @ConfigEntry.Category(value = "general")
+    @ConfigEntry.Gui.Tooltip()
+    public Integer maxEntityCount = 32;
+    @ConfigEntry.Category(value = "general")
+    @ConfigEntry.Gui.Tooltip()
     public Boolean applyMaxEntityCountGlobally = true;
 
+    @ConfigEntry.Category(value = "entities")
+    public Map<String, Integer> entityMaxCounts = new TreeMap<>();
+
+    @ConfigEntry.Category(value = "general")
     @ConfigEntry.Gui.Excluded
     public static TooManyEntitiesConfig instance = null;
+
+    public TooManyEntitiesConfig() {
+        Registries.ENTITY_TYPE.stream().forEach(entityType -> entityMaxCounts.put(entityType.getTranslationKey(), 0));
+    }
+
+    @Override
+    public void validatePostLoad() {
+        if (maxEntityCount < 0) {
+            maxEntityCount = 0;
+        }
+
+        for (Map.Entry<String, Integer> entry : entityMaxCounts.entrySet()) {
+            if (entry.getValue() < 0) {
+                entry.setValue(0);
+            }
+        }
+    }
 }
