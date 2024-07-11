d09abf56ac4192eddc6df674a4a626747c19949c
Discusser <47938380+Discusser@users.noreply.github.com>
2024-07-11 15:17:26 +0200

    Fix entity counting when EntityCulling is not installed

--- src/client/java/io/github/discusser/toomanyentities/mixin/client/WorldRendererMixin.java
+++ src/client/java/io/github/discusser/toomanyentities/mixin/client/WorldRendererMixin.java
@@ -2,18 +2,24 @@ package io.github.discusser.toomanyentities.mixin.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import io.github.discusser.toomanyentities.TooManyEntitiesClient;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
+    @Shadow
+    private int regularEntityCount;
+
     @Inject(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/WorldRenderer;regularEntityCount:I", ordinal = 1, shift = At.Shift.AFTER))
     private void afterEntityCountIncrement(CallbackInfo info, @Local Entity entity) {
         String key = entity.getType().getTranslationKey();
+        System.out.println(regularEntityCount + "\t and \t" + TooManyEntitiesClient.entityCounts.get("entity.minecraft.chicken"));
         TooManyEntitiesClient.entityCounts.put(key, TooManyEntitiesClient.entityCounts.getOrDefault(key, 0) + 1);
     }
 }
