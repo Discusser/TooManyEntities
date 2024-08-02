b3bd65ed0fc206e9fa0232f050ef699b239fcaf5
Discusser <47938380+Discusser@users.noreply.github.com>
2024-08-02 10:12:04 +0200

    Create template architectury project

---
+++ common/src/main/java/io/github/discusser/toomanyentities/ExampleMod.java
@@ -0,0 +1,9 @@
+package io.github.discusser.toomanyentities;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "too-many-entities";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
